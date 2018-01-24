a3ebca45e41a2d3cffe3cd99235c903d20c2cdac
Tomáš Hapl <tomas.hapl@gmail.com>
2018-01-24 14:23:00 +0100

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react"
-import Header from "./components/Header/Header"
-import BasicParameters from "./components/BasicParameters/BasicParameters"
-import Parts from "./components/Parts/Parts"
-import Order from "./components/Order/Order"
 
 class App extends React.Component {
 
